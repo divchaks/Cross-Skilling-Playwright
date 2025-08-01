5d7cd3428ef3eac06ad2f27967e642c044f33843
divchaks <divyachaks28@gmail.com>
2025-08-01 10:34:15 +0100

    Update Playwright config file to enable video recording, screenshots, and non-headless mode

--- playwright.config.js
+++ playwright.config.js
@@ -31,6 +31,9 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    headless:false,
+    video:'on',
+    screenshot:'on',
   },
 
   /* Configure projects for major browsers */
@@ -40,7 +43,7 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
+    /*{
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
@@ -48,7 +51,7 @@ export default defineConfig({
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
-    },
+    },*/
 
     /* Test against mobile viewports. */
     // {
